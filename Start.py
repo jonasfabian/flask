39452b5927cd5fd3108d8dc09a16297374caf53f
jonas.fabian <jonas.fabian@fhnw.ch>
2019-09-23 13:32:55 +0200

    code structure improvement

--- Start.py
+++ Start.py
@@ -14,15 +14,16 @@ dataBase = mysql.connector.connect(
 )
 
 
-@app.route("/", methods=['GET'])
+@app.route("/getUser", methods=['GET'])
 def getUserById():
     cursor = dataBase.cursor()
     cursor.execute("SELECT * FROM user WHERE id = %s", (request.args.get('id'),))
     rv = cursor.fetchall()
-    payload=[]
-    content={}
+    payload = []
+    content = {}
     for result in rv:
-        content = {'id': result[0], 'firstName': result[1], 'lastName': result[2], 'email': result[3], 'username': result[4], 'avatarVersion': result[5]}
+        content = {'id': result[0], 'firstName': result[1], 'lastName': result[2], 'email': result[3],
+                   'username': result[4], 'avatarVersion': result[5]}
         payload.append(content)
         content = {}
     return jsonify(payload)
