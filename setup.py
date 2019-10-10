aea690e5e5ea64f65091e7bd44aef17b20ae14e4
Jonas.Fabian <jonas.fabian@stud.bbbaden.ch>
2019-10-10 15:36:49 +0200

    wip

--- setup.py
+++ setup.py
@@ -28,70 +28,21 @@ def createDatabase():
 
 
 def createTables():
-    # Table audio
     cursor.execute(
-        """CREATE TABLE IF NOT EXISTS audio (
+        """CREATE TABLE IF NOT EXISTS audioSnippets (
         id BIGINT NOT NULL AUTO_INCREMENT,
-        path VARCHAR(200),
-        fileId BIGINT NOT NULL,
+        timelineId VARCHAR(45),
+        time FLOAT NOT NULL,
         PRIMARY KEY (id))""")
 
-    # Table audio
     cursor.execute(
-        """CREATE TABLE IF NOT EXISTS textAudioIndex (
+        """CREATE TABLE IF NOT EXISTS speaker (
         id BIGINT NOT NULL AUTO_INCREMENT,
-        samplingRate INT,
-        textStartPos INT,
-        textEndPos INT,
-        audioStartPos INT,
-        audioEndPos INT,
-        speakerKey INT,
-        labeled INT default 0,
-        correct INT default 0,
-        wrong INT default 0,
-        transcript_file_id INT,
+        speakerId VARCHAR(45),
+        sex VARCHAR(45),
+        languageUsed VARCHAR(45),
+        dialect VARCHAR(45),
         PRIMARY KEY (id))""")
-
-    # Table transcript
-    cursor.execute(
-        """CREATE TABLE IF NOT EXISTS transcript (
-        id BIGINT NOT NULL AUTO_INCREMENT,
-        text MEDIUMTEXT CHARACTER SET utf8,
-        fileId BIGINT NOT NULL,
-        PRIMARY KEY (id))""")
-
-    # Table user
-    cursor.execute(
-        """CREATE TABLE IF NOT EXISTS user (
-        id BIGINT NOT NULL AUTO_INCREMENT,
-        firstName VARCHAR (100),
-        lastName VARCHAR (100),
-        email VARCHAR (100),
-        username VARCHAR(100),
-        avatarVersion INT,
-        password VARCHAR (100),
-        PRIMARY KEY (id),
-        UNIQUE KEY email (email))""")
-
-    # Table userAndTextAudioIndex
-    cursor.execute(
-        """CREATE TABLE IF NOT EXISTS userAndTextAudioIndex (
-        id BIGINT NOT NULL AUTO_INCREMENT,
-        userId INT,
-        textAudioIndexId INT,
-        time TIMESTAMP,
-        PRIMARY KEY (id),
-        CONSTRAINT uni UNIQUE (userId, textAudioIndexId))""")
-
-    # Table avatar
-    cursor.execute(
-        """CREATE TABLE IF NOT EXISTS avatar (
-        id BIGINT NOT NULL AUTO_INCREMENT,
-        userId INT,
-        avatar BLOB,
-        PRIMARY KEY (id),
-        UNIQUE KEY userId (userId))""")
-
 
 def main():
     dropDatabase()
